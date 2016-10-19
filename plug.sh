06cb8e57631b76f3a16adec02cab4bec95c37ef8
Jackong <daisy@Jackong.local>
2016-10-19 11:06:28 +0800

    add ctags

--- plug.sh
+++ plug.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+brew tap universal-ctags/universal-ctags
+brew install --HEAD universal-ctags
